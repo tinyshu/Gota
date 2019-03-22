25699383d8261b3abc94f1d6ae21dd2f0824a83f
shuyi <shuyi@kingsoft.com>
2019-03-22 18:24:02 +0800

    增加lua层socket proto的type类型定义

--- src/script/auth_server/main.lua
+++ src/script/auth_server/main.lua
@@ -1,8 +1,13 @@
---初始化日志模块
+
+--初始化日志模块必须放在最前面，避免加载其他模块打log而没有初始化log模块
+--true表示写文件同时输出到控制台
 Logger_wrapper.init("logger/auth/", "auth", true)
 
---连接到auth db
 require("database/mysql_auth_center")
+local socket_proto_type = require("socket_proto_type")
+local stype = require("service_type")
+local config = require("conf")
+--[[
 local socket_type = {
 	TCP_SOCKET = 0,  --tcp
 	WEB_SOCKET = 1,  --websocket
@@ -13,19 +18,20 @@ local proto_type = {
 	PROTO_JSON = 1,
 }
 
-session_wrapper.set_socket_and_proto_type(socket_type.TCP_SOCKET,proto_type.PROTO_BUF)
+]]
+
+
+session_wrapper.set_socket_and_proto_type(socket_proto_type.socket_type.TCP_SOCKET,socket_proto_type.proto_type.PROTO_BUF)
 --session_wrapper.set_socket_and_proto_type(socket_type.TCP_SOCKET,proto_type.PROTO_JSON)
 --protobuf协议，注册cmd
-if session_wrapper.get_proto_type() == proto_type.PROTO_BUF then
+if session_wrapper.get_proto_type() == socket_proto_type.proto_type.PROTO_BUF then
    local cmd_name_map = require("cmd_name_map")
    if cmd_name_map then
 	proto_mgr_wrapper.register_protobuf_cmd(cmd_name_map)
    end
 end
 
---加载配置
-local stype = require("service_type")
-local config = require("conf")
+
 local servers = config.servers
 --开启网络服务
 print("start gateway service success ip:".. servers[stype.AuthSerser].ip,"port:"..servers[stype.AuthSerser].port)
